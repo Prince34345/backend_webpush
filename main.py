caad9584d39277f951a9220665a48e53c77a5a23
Prince34345 <priyanshudhawan55@gmail.com>
2025-08-31 23:52:41 +0530

    minor issue resolved

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["https://frontend-webpush.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
